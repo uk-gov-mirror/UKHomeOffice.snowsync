7e60ec541cf89583babe64a1a8871ea5182144da
Onur Yelekci <onur.yelekci@digital.homeoffice.gov.uk>
2020-12-12 15:34:50 +0000

    Improve caller func after e2e tests

--- cmd/caller/main.go
+++ cmd/caller/main.go
@@ -1,14 +1,14 @@
+// Function caller hands over to package caller.
 package main
 
 import (
 	"github.com/aws/aws-lambda-go/lambda"
 
-	"github.com/UKHomeOffice/snowsync/internal/caller"
-	"github.com/UKHomeOffice/snowsync/internal/client"
+	"github.com/UKHomeOffice/snowsync/pkg/caller"
 )
 
-func handler(e client.Envelope) (string, error) {
-	return caller.Call(e)
+func handler(ms map[string]interface{}) (string, error) {
+	return caller.CallSNOW(ms)
 }
 
 func main() {
